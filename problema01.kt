e110ce40797c20cc479e2b14f7a343ca66bd946c
CUELLAR <L24230239@minatitlan.tecnm.mx>
2024-11-24 14:23:49 -0600

    Update problema01.kt

--- problema01.kt
+++ problema01.kt
@@ -1,6 +1,6 @@
 #1. Fibonacci inverso: Implementa una función que calcule los primeros N números de la serie de Fibonacci en orden inverso.
 
-def fibonacci_inverso(n):
+def fibonacciinverso(n):
     fib = [0, 1]
     for i in range(2, n):
         fib.append(fib[-1] + fib[-2])
