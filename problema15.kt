8f2312d4b3ac6ff858704148adeab3e6a260910d
CUELLAR <L24230239@minatitlan.tecnm.mx>
2024-11-24 16:15:06 -0600

    Create problema15.kt

---
+++ problema15.kt
@@ -0,0 +1,12 @@
+#15. Determinante de una matriz: Crea una función que calcule el
+#determinante de matrices de hasta 4x4.
+
+from sympy import Matrix
+
+def determinante_matriz(matriz):
+    return Matrix(matriz).det()
+
+#Ejemplo:
+matriz = [[1, 2, 3], [0, 1, 4], [5, 6, 0]]
+print("Determinante:", determinante_matriz(matriz))
+
